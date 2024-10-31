7f508eb734777ebe40f2e3fa054bf85e8a9f445a
T-Anh-k4 <146644118+T-Anh-k4@users.noreply.github.com>
2024-10-31 13:59:51 +0700

    tanh

--- QLBH/QLBH/obj/Debug/net8.0/QLBH.AssemblyInfo.cs
+++ QLBH/QLBH/obj/Debug/net8.0/QLBH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLBH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb5c573b2ce957fc5c96fc9d4d1d8c3f568bdfde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21d8aba4489fe5df9bfb986717e57d29aae86551")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLBH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLBH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
