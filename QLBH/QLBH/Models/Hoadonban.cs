0148f905b51796e08cb823eeb7a42e0ea5aea227
hoangvn03 <hoangnguyenngoc2k3@gmail.gmailcom>
2024-11-01 18:41:13 +0700

    up

--- QLBH/QLBH/Models/Hoadonban.cs
+++ QLBH/QLBH/Models/Hoadonban.cs
@@ -1,18 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace QLBH.Models;
 
 public partial class Hoadonban
 {
     public int SoHdb { get; set; }
-
+    [Required(ErrorMessage = "Xin vui lòng nhập mã khách hàng.")]
     public int? MaKh { get; set; }
-
+    [Required(ErrorMessage = "Xin vui lòng nhập ngày đặt hàng.")]
     public DateTime? NgayDatHang { get; set; }
-
+    [Required(ErrorMessage = "Xin vui lòng nhập tổng tiền.")]
     public decimal? TongTien { get; set; }
-
+    [Required(ErrorMessage = "Xin vui lòng nhập trạng thái.")]
     public string? TrangThaiTt { get; set; }
 
     public virtual ICollection<Chitiethoadon> Chitiethoadons { get; set; } = new List<Chitiethoadon>();
