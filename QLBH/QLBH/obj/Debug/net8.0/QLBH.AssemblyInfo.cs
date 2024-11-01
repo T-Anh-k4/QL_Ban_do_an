b7e3be3a49083dad577f3b3ecee76d37f0f413bf
hoangvn03 <hoangnguyenngoc2k3@gmail.gmailcom>
2024-11-01 17:31:45 +0700

    up nốt phần đang nhập đăng ký

--- QLBH/QLBH/obj/Debug/net8.0/QLBH.AssemblyInfo.cs
+++ QLBH/QLBH/obj/Debug/net8.0/QLBH.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QLBH")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a33ed49f525b25c85fbc198530b1e24ca55faa1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8363ac98bfe4da5a9a2732a927b1200b79c27d5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("QLBH")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QLBH")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
